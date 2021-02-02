aefb924925d18c246c51319436e25d551cf2fc9f
Zak Hayek <58797883+zakhayek@users.noreply.github.com>
2021-02-02 11:19:01 -0800

    Delete reviews-service-master directory

--- reviews-service-master/client/setupTests.js
+++
@@ -1,8 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'regenerator-runtime/runtime';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-const noop = () => {};
-Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
--- reviews-service-master/spec/seed.test.js
+++
@@ -1,5 +0,0 @@
-const generateReviews = require('../database/helpers.js');
-
-test('Length of array is 100', () => {
-  expect(generateReviews().length).toEqual(100);
-});
