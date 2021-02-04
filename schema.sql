156e2f436fede50166eadea433ce0788379094e2
Patrice Cottman <patrice.cottman@gmail.com>
2021-02-04 13:11:52 -0500

    start work on sql schema

---
+++ schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS  reviews;
+
+CREATE DATABASE reviews;
+
+USE reviews;
+
+-- CREATE TABLE cows (
+-- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- cowname varchar(200) NOT NULL,
+-- cowdescription varchar(200) NOT NULL
+-- );
+
+CREATE TABLE productID(
+  _id INT NOT NULL PRIMARY KEY,
+
+
+);
+
+CREATE TABLE
