6d702c0c299e156c90c0d047e72def2a68f88d5d
Patrice Cottman <patrice.cottman@gmail.com>
2021-02-10 00:04:16 -0500

    changes

--- database/queries.js
+++ database/queries.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
 const { Pool, Client }  = require('pg');
-const connectionString = 'postgresql://pcottman@localhost:5432/postgres';
+const connectionString = `postgresql://student:${process.env.STUDENT_PASSWORD}@ec2-44-192-132-165.compute-1.amazonaws.com:5432/postgres`;
 const pool = new Pool({
  connectionString,
 })
 
-// pool.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-//   pool.end()
-// })
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
 
 module.exports = pool;
