a984cb0854b656e8616784db15ff2e4b2743e66d
Patrice Cottman <patrice.cottman@gmail.com>
2021-02-11 00:22:50 -0500

    changes

--- server/newrelic.js
+++ server/newrelic.js
@@ -1,6 +1,5 @@
-'use strict'
+//'use strict'
 require('dotenv').config();
-
 /**
  * New Relic agent configuration.
  *
@@ -15,7 +14,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key:`${process.env.RELIC_SDC}`,
+  license_key: process.env.NEW_RELIC,
   /**
    * This setting controls distributed tracing.
    * Distributed tracing lets you see the path that a request takes through your
